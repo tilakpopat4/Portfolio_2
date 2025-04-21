93c9624cf8a0d92acad48de4e375a07dc61f52c1
Tilak Popat <tilakpopat2007@gmail.com>
2025-04-21 16:35:11 +0530

    photo_library.py

--- photo_library.py
+++ photo_library.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Tilak's Photo Gallery", layout="wide")
 st.title("📸 Tilak's Photography Library")
 
 # Load image folder
-image_folder = "photos"
+image_folder = "images"
 image_files = [f for f in os.listdir(image_folder) if f.endswith((".png", ".jpg", ".jpeg"))]
 
 # Sort to keep consistent order
